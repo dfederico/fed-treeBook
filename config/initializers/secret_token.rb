15bdc391065403f20fb71e706527e5a6069feb1b
Dan Federico <dfederico7@gmail.com>
2014-11-02 13:49:51 -0600

    pushing files to the hub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreeBook::Application.config.secret_token = 'efd75858243edca1c35995571f621330364dba07d7d08e1dd7be7eb8ec735762ef04c3f689c1af8f4c5014c4da8cbe82bd9698af3144e7c094aabdf8296c2005'
